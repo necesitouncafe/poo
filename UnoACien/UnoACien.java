9101d5f07a3a5e16428ad355aae010e789ad4703
necesitouncafe <patri.cursos.vigo@gmail.com>
2026-06-23 12:57:51 +0200

    UnoACien

---
+++ UnoACien/UnoACien.java
@@ -0,0 +1,15 @@
+package UnoACien;
+
+public class UnoACien {
+    public static void main(String[] args){
+
+    int i =1;
+
+    while (i <=100) {
+        System.out.println(i);
+        i ++;
+    }
+
+    }
+    
+}
